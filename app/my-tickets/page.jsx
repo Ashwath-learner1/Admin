8223c939d41792136230c13cedc3c39a0910edc8
SauravSarkar123 <sauravsarkar421@gmail.com>
2023-09-27 12:48:58 +0530

    mm

--- app/my-tickets/page.jsx
+++ app/my-tickets/page.jsx
@@ -88,7 +88,7 @@ export default function MyTickets() {
 
 
         <div className='flex justify-between '>
-          <p className='text-black text-left block dark:text-white text-2xl' >My Tickets</p>
+          <p className='text-black text-left ml-7 md:ml-7 block dark:text-white text-2xl' >My Tickets</p>
           <Link
             href="#"
             className="inline-flex items-center  justify-center  rounded-md bg-meta-6 py-2 px-10 text-center font-medium text-black hover:bg-opacity-90 lg:px-8 xl:px-10"
